35d93ed3546ef11997824afe7dce0fd31fc5bc04
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2022-09-05 15:31:31 +0530

    Arup Mandal

---
+++ README.md
@@ -0,0 +1,19 @@
+# Utube Download 😉
+## Prerequisite
+    ffmpeg
+  
+    
+## install dependencies
+    pip3 install -r requirements.txt
+
+
+## Setup Bot
+    - Change configuration config.py  File
+    - install dependencies
+    - python3 -m bot
+    
+## Thanks ❤️
+* [TG Free Bots](https://telegram.dog/TG_Free_bots) for their [Utube Download](https://telegram.me/Utube_downloadbot)
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Arup-Bots/YouTybe-Downloader)
---
+++ plugins/start.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/arupbots")],
+        [InlineKeyboardButton("All our FREE BOTS", url="https://t.me/arupbots/3")]
+
+
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b> Welcome To Youtube Downloader </b> This Bot Created By @tech_arup \n help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
+    
+    
